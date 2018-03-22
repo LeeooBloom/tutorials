85a988d3078b7d7bff38cb854bc3da79a0eba661
leobloom <mikhail.rozhdestvenskiy@gost-group.com>
2018-03-22 22:32:27 +0300

    betta

--- boston_houses.py
+++ boston_houses.py
@@ -1,12 +1,10 @@
-import urllib
-from urllib import request
 import numpy as np
 
-f = urllib.request.urlopen('https://stepic.org/media/attachments/lesson/16462/boston_houses.csv')  # open file from URL
-data = np.loadtxt(f, delimiter=',', skiprows=1)  # load data to work with
+data = np.loadtxt('boston_houses.csv', delimiter=',', skiprows=1)  # load data to work with
 
 def main ():
     count = data.shape[0]
+    y =  data[:,0]
     x1 = data[:,1]
     x2 = data[:,2]
     x3 = data[:,3]
@@ -15,6 +13,13 @@ def main ():
     x6 = data[:,6]
     ones = np.ones(count)
     X = np.column_stack((ones,x1,x2,x3,x4,x5,x6))
+    Xt = np.transpose(X)
+    XtX = np.matrix.dot(Xt,X)
+    XtXinv = np.linalg.inv(XtX)
+    XtY = np.matrix.dot(Xt,y)
+    beta = np.matrix.dot(XtXinv,XtY)
+    print beta
+
 
 
 if __name__ == "__main__":
